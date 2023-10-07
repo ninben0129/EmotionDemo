3d08d78ccbae6dcdd7471210f94282b108b8038f
Masahiro Tanaka <tanaka.masahiro.r6@dc.tohoku.ac.jp>
2023-10-07 15:39:07 +0900

    add DrawAnimationClips.cs

---
+++ Assets/Editor/DrawAnimationClips.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class Copy
+{
+    [MenuItem("Tools/Animation/Copy")]
+    static void AssetCopy ()
+    {
+        // AnimationClipを持つFBXのパス
+        string fbxPath = "Assets/Animation/new/anger_H2.fbx";
+        // AnimationClipのファイル名
+        string clipName = "Armature|2022-03-12_CK_anger_1_H_remap";
+        // AnimationClipの出力先
+        string exportPath = "Assets/Animation/new/anger_H.anim";
+
+        string tempExportedClip = "Assets/Animation/new/tempClip.anim";
+
+        // AnimationClipの取得
+        var animations = AssetDatabase.LoadAllAssetsAtPath (fbxPath);
+        var originalClip = System.Array.Find<Object> (animations, item =>
+            item is AnimationClip && item.name == clipName
+        );
+	
+        // AnimationClipをコピーして出力(ユニークなuuid)
+        var copyClip = Object.Instantiate (originalClip);
+        AssetDatabase.CreateAsset (copyClip, tempExportedClip);
+
+        // AnimationClipのコピー（固定化したuuid）
+        File.Copy (tempExportedClip, exportPath, true);
+        File.Delete (tempExportedClip);
+
+        // AssetDatabaseリフレッシュ
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh ();
+    }
+}
