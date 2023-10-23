6b4a7cf271263351252cc494e8ba426122d80d03
Masahiro Tanaka <83493512+ninben0129@users.noreply.github.com>
2023-10-23 21:56:28 +0900

    update

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -11,79 +11,112 @@ public class ButtonClick : MonoBehaviour
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Disgust();
     }
+
     public void FearClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Fear();
     }
+
     public void GratitudeClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Gratitude();
     }
+
     public void GuiltClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Guilt();
     }
+
     public void JealousyClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Jealousy();
     }
+
     public void JoyClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Joy();
     }
+
     public void PrideClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Pride();
     }
+
     public void SadnessClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Sadness();
     }
+
     public void ShameClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Shame();
     }
+
     public void SurpriseClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Surprise();
     }
+
     public void NeutralClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Neutral();
     }
+
     public void HighClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.High();
     }
+
     public void MiddleClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Middle();
     }
+
     public void LowClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Low();
     }
+
     public void AngerClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Anger();
     }
+
     public void ContemptClick()
     {
         _transition = GameObject.Find(Avatar).GetComponent<Transition>();
         _transition.Contempt();
+    }
+
+    public void Scenario1Click()
+    {
+        _transition = GameObject.Find(Avatar).GetComponent<Transition>();
+        _transition.Scenario1();
+    }
+
+    public void Scenario2Click()
+    {
+        _transition = GameObject.Find(Avatar).GetComponent<Transition>();
+        _transition.Scenario2();
+    }
+
+    public void Scenario3Click()
+    {
+        _transition = GameObject.Find(Avatar).GetComponent<Transition>();
+        _transition.Scenario3();
     }
 }
--- Assets/Scripts/Transition.cs
+++ Assets/Scripts/Transition.cs
@@ -6,16 +6,18 @@ public class Transition : MonoBehaviour
     private Animator _animator;
     private Text _eText;
     private Text _lText;
+    private Text _sText;
 
     private static readonly int Trans = Animator.StringToHash("trans");
     private static readonly int Level = Animator.StringToHash("level");
+    private static readonly int Scenario = Animator.StringToHash("scenario");
 
     // Start is called before the first frame update
     private void Start()
     {
         _animator = GetComponent<Animator>();
     }
-    
+
     public void Neutral()
     {
         _animator.SetInteger(Trans, 0);
@@ -25,94 +27,130 @@ public class Transition : MonoBehaviour
         this._lText = GameObject.Find("LevelText").GetComponent<Text>();
         _lText.text = "Middle";
     }
+
     public void Anger()
     {
         _animator.SetInteger(Trans, 1);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Anger";
     }
+
     public void Contempt()
     {
         _animator.SetInteger(Trans, 2);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Contempt";
     }
+
     public void Disgust()
     {
         _animator.SetInteger(Trans, 3);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Disgust";
     }
+
     public void Fear()
     {
         _animator.SetInteger(Trans, 4);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Fear";
     }
+
     public void Gratitude()
     {
         _animator.SetInteger(Trans, 5);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Gratitude";
     }
+
     public void Guilt()
     {
         _animator.SetInteger(Trans, 6);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Guilt";
     }
+
     public void Jealousy()
     {
         _animator.SetInteger(Trans, 7);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Jealousy";
     }
+
     public void Joy()
     {
         _animator.SetInteger(Trans, 8);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Joy";
     }
+
     public void Pride()
     {
         _animator.SetInteger(Trans, 9);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Pride";
     }
+
     public void Sadness()
     {
         _animator.SetInteger(Trans, 10);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Sadness";
     }
+
     public void Shame()
     {
         _animator.SetInteger(Trans, 11);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Shame";
     }
+
     public void Surprise()
     {
         _animator.SetInteger(Trans, 12);
         this._eText = GameObject.Find("EmoText").GetComponent<Text>();
         _eText.text = "Surprise";
     }
+
     public void High()
     {
         _animator.SetInteger(Level, 3);
         this._lText = GameObject.Find("LevelText").GetComponent<Text>();
         _lText.text = "High";
     }
+
     public void Middle()
     {
         _animator.SetInteger(Level, 2);
         this._lText = GameObject.Find("LevelText").GetComponent<Text>();
         _lText.text = "Middle";
     }
+
     public void Low()
     {
         _animator.SetInteger(Level, 1);
         this._lText = GameObject.Find("LevelText").GetComponent<Text>();
         _lText.text = "Low";
+    }
+
+    public void Scenario1()
+    {
+        _animator.SetInteger(Scenario, 1);
+        this._sText = GameObject.Find("ScenarioText").GetComponent<Text>();
+        _sText.text = "Scenario1";
+    }
+
+    public void Scenario2()
+    {
+        _animator.SetInteger(Scenario, 2);
+        this._sText = GameObject.Find("ScenarioText").GetComponent<Text>();
+        _sText.text = "Scenario2";
+    }
+
+    public void Scenario3()
+    {
+        _animator.SetInteger(Scenario, 3);
+        this._sText = GameObject.Find("ScenarioText").GetComponent<Text>();
+        _sText.text = "Scenario3";
     }
 }
